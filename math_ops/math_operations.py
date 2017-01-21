4cae56ca641f00dbe6eeb5f561dc764c0042d815
danielwangai <mainad57@gmail.com>
2017-01-21 15:34:18 +0300

    Add function for subtraction

--- math_ops/math_operations.py
+++ math_ops/math_operations.py
@@ -4,3 +4,6 @@ def get_numbers(number_1, number_2):
 
 def add_numbers(list_of_numbers):
     return sum(list_of_numbers)
+
+def subtract_numbers(list_of_numbers):
+    return (list_of_numbers[0] - list_of_numbers[1])
