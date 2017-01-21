67133276be173568fcc8330350e1c883aba250b6
danielwangai <mainad57@gmail.com>
2017-01-21 12:13:11 +0300

    Add test ensuring numbers are not added to strings

--- math_ops/test_math_operations.py
+++ math_ops/test_math_operations.py
@@ -5,6 +5,12 @@ class MathOps(unittest.TestCase):
     def test_method_adds_correcty(self):
         self.assertEqual(add_numbers(3, 4), 7)
 
+    def test_add_non_number_params(self):
+        with self.assertRaises(TypeError):
+            add_numbers("2",2)
+        with self.assertRaises(TypeError):
+            add_numbers(2,"2")
+
 
 if __name__== '__main__':
     unittest.main()
