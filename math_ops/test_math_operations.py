639f06d4bf6bc50da9668d8bdbf12c642ae7fe0d
danielwangai <mainad57@gmail.com>
2017-01-21 13:08:14 +0300

    refactor tests

--- math_ops/test_math_operations.py
+++ math_ops/test_math_operations.py
@@ -1,15 +1,20 @@
 import unittest
-from math_operations import add_numbers
+from math_operations import add_numbers, get_numbers
 
 class MathOps(unittest.TestCase):
+
+    def setUp(self):
+        self.list_of_numbers = get_numbers(2,3)
+        self.list_num_string = get_numbers(2,"3")
+        self.list_string_num = get_numbers("2",3)
     def test_method_adds_correcty(self):
-        self.assertEqual(add_numbers(3, 4), 7)
+        self.assertEqual(add_numbers(self.list_of_numbers), 5)
 
     def test_add_non_number_params(self):
         with self.assertRaises(TypeError):
-            add_numbers("2",2)
+            add_numbers(self.list_num_string)
         with self.assertRaises(TypeError):
-            add_numbers(2,"2")
+            add_numbers(self.list_string_num)
 
 
 if __name__== '__main__':
