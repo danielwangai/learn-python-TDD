2fc8cd255f7171e217f102fd27a895de26746b3f
danielwangai <mainad57@gmail.com>
2017-01-21 15:35:43 +0300

    Add method for division

--- math_ops/math_operations.py
+++ math_ops/math_operations.py
@@ -10,3 +10,9 @@ def subtract_numbers(list_of_numbers):
 
 def multiply_numbers(list_of_numbers):
     return list_of_numbers[0]*list_of_numbers[1]
+
+def divide_numbers(list_of_numbers):
+    if list_of_numbers[1] == 0:
+        return "Error"
+    else:
+        return list_of_numbers[0]/list_of_numbers[1]
