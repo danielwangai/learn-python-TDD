fe3c5fe78932158bb1dd065705f792ad0040d7ac
danielwangai <mainad57@gmail.com>
2017-01-21 13:41:04 +0300

    Add test to check that both inputs are not int or float.

--- math_ops/test_math_operations.py
+++ math_ops/test_math_operations.py
@@ -7,6 +7,7 @@ class MathOps(unittest.TestCase):
         self.list_of_numbers = get_numbers(2,3)
         self.list_num_string = get_numbers(2,"3")
         self.list_string_num = get_numbers("2",3)
+        self.list_all_string = get_numbers("2","3")
     def test_method_adds_correcty(self):
         self.assertEqual(add_numbers(self.list_of_numbers), 5)
 
@@ -23,9 +24,15 @@ class MathOps(unittest.TestCase):
         self.assertTrue(type_number_1 and type_number_2)
 
     def test_data_type_of_atleast_one_input_not_int_or_float(self):
-        # tests at least one of inputs is of type int or float
+        # tests at least one of the inputs is of type int or float
         type_number_1 = type(self.list_num_string[0]) == int or type(self.list_num_string[0]) == float
         type_number_2 = type(self.list_num_string[1]) == int or type(self.list_num_string[1]) == float
+        self.assertFalse(type_number_1 and type_number_2)
+
+    def test_data_type_of_both_inputs_are_not_int_or_float(self):
+        # tests at least one of inputs are of type int or float
+        type_number_1 = type(self.list_all_string[0]) == int or type(self.list_all_string[0]) == float
+        type_number_2 = type(self.list_all_string[1]) == int or type(self.list_all_string[1]) == float
         self.assertFalse(type_number_1 and type_number_2)
 
 
