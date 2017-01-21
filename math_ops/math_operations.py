1227ca78571a551ce883237878e3f1bf329b7493
danielwangai <mainad57@gmail.com>
2017-01-21 15:33:55 +0300

    Add function for summing numbers

--- math_ops/math_operations.py
+++ math_ops/math_operations.py
@@ -1,3 +1,6 @@
 def get_numbers(number_1, number_2):
     # returns list of numbers, ready for operation
     return [number_1, number_2]
+
+def add_numbers(list_of_numbers):
+    return sum(list_of_numbers)
